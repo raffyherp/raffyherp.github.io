a20c9111c1908ead86f9cec9d70130228d00d2e7
raffyherp <63196437+raffyherp@users.noreply.github.com>
2021-09-04 13:42:29 +0700

    Initial commit

---
+++ assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "raffyherp", quotes: "as long as i live, i love you" },
+    { author: "raffyherp", quotes: "as long as i live, i love you" },
+    { author: "raffyherp", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "raffyherp", quotes: "I love you." },
+    { author: "raffyherp", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "raffyherp", quotes: "I love you." },
+    { author: "raffyherp", quotes: "I'm sorry for loving you." },
+];
---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Reyni Nursafitri", "Ayangg <3", "Sayang", "Sayangku", "Cintakuu"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
